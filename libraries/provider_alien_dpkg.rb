05e10e541e9fde50a5828bb0df564d2424184814
David Kinzer <dtkinzer@gmail.com>
2014-08-07 11:34:02 -0400

    Add AlienDpkg Provider.
    This commit adds an AlienDpkg provider for the package resource. The
    main purpose of this provider is to automatically substitute an rpm
    package source path with a debian source path.  Also if the debian
    package has not been generated, the provider automatically attempts to
    convert the rpm package to a debian package.

---
+++ libraries/provider_alien_dpkg.rb
@@ -0,0 +1,37 @@
+# Cookbook Name:: oracle-instantclient
+# Provider:: AlienDpkg
+# Author:: David Kinzer <dtkinzer@gmail.com>
+#
+# Copyright (C) 2014 David Kinzer
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'chef/provider/package'
+require 'chef/provider/package/dpkg'
+require_relative 'mixin_alien_converter'
+
+class Chef
+  class Provider
+    # Alien wrapper to the Dpkg package provider.
+    class AlienDpkg < Chef::Provider::Package::Dpkg
+      include Chef::Mixin::AlienConverter
+
+      def initialize(new_resource, run_context)
+        super
+        Chef::Log.info("Run Context #{run_context}")
+        new_resource.source package_debian_source
+        log_convert
+      end
+    end
+  end
+end
---
+++ test/libraries/provider_alien_dpkg.rb
@@ -0,0 +1,43 @@
+# Cookbook Name:: oracle-instantclient
+# Provider:: AlienDpkg
+# Author:: David Kinzer <dtkinzer@gmail.com>
+#
+# Copyright (C) 2014 David Kinzer
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+require 'minitest/autorun'
+require 'chef/resource/dpkg_package'
+require_relative '../../libraries/provider_alien_dpkg'
+
+# Override the shell_out method.
+class Chef::Provider::AlienDpkg
+  def shell_out!(command, options = [])
+    command
+  end
+end
+
+# Unit tests for Chef::Provider::AlienConvert
+class TestProviderAlienDpkg < MiniTest::Test
+  def setup
+    resource = Chef::Resource::DpkgPackage.new 'A-fake-rpm-package'
+    resource.source '/foo/bar/fake-package-1.2.3.4-0.x86_64.rpm'
+    @provider = Chef::Provider::AlienDpkg.new resource, nil
+  end
+
+  def test_source_initialization
+    expected = '/foo/bar/fake-package_1.2.3.4-0_amd64.deb'
+    actual = @provider.new_resource.source
+    assert_equal expected, actual
+  end
+end
